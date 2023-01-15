b9fc1daef0adffd0c4bfffb1b4bf9a62bfafbf38
Igor Xavier <igorxavier.ixds@gmail.com>
2023-01-15 20:13:33 -0300

    feat: adicionado arquivo robots.txt & melhoria de metatags

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,14 +49,14 @@ export default function Home() {
             <meta property="og:title" content="Igor Xavier - Desenvolvedor Front-end"/>
             <meta property="og:description" content="Especializado em desenvolvimento de aplicações front end (ocasionalmente também criando designs) de qualidade, 
             visando performace, modernas, responsivas, com um bom SEO & UI/UX."/>
-            <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+            <meta property="og:image" content="/meta.png"/>
 
             <meta property="twitter:card" content="summary_large_image"/>
             <meta property="twitter:url" content="https://metatags.io/"/>
             <meta property="twitter:title" content="Igor Xavier - Desenvolvedor Front-end"/>
             <meta property="twitter:description" content="Especializado em desenvolvimento de aplicações front end (ocasionalmente também criando designs) de qualidade, 
             visando performace, modernas, responsivas, com um bom SEO & UI/UX."/>
-            <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+            <meta property="twitter:image" content="/meta.png"/>
         </Head>
         <Navbar/>
         <main className="overflow-hidden pb-10 w-full relative">
